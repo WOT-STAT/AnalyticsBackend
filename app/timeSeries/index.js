9a55fcf2adfa497aae5cfc751104dc232b231603
Andrei <soprachev@mail.ru>
2021-09-06 05:26:14 +0300

    add events loading

--- app/timeSeries/index.js
+++ app/timeSeries/index.js
@@ -6,7 +6,7 @@ import { Query, knex } from '../dbProvider.js'
 const router = Router()
 const { query } = validator;
 
-router.get('/load',
+router.get('/battles',
     query('limit').isInt({ min: 0, max: 100 }).toInt().optional(),
     validationMustBe,
     async (req, res) => {
@@ -30,6 +30,30 @@ router.get('/load',
                 .from(start.distinct('*').as('start'))
                 .leftJoin(result, { 'start.id': 'result.onBattleStart_id' }))
         res.json(r)
+    })
+
+
+router.get('/battleEvents',
+    query('battleid').isUUID(),
+    validationMustBe,
+    async (req, res) => {
+        const battleId = req.query.battleid
+
+        const onBattleStarat = knex.from('Event_OnBattleStart').where('id', '=', battleId).orderBy('dateTime', 'desc').distinct('*')
+        const onShot = knex.from('Event_OnShot').where('onBattleStart_id', '=', battleId).orderBy('dateTime', 'desc').distinct('*')
+        const onBattleResult = knex.from('Event_OnBattleResult').where('onBattleStart_id', '=', battleId).orderBy('dateTime', 'desc').distinct('*')
+
+        const result = await Promise.all([
+            onBattleStarat,
+            onShot,
+            onBattleResult
+        ].map(t => Query(t)))
+
+        res.json({
+            onBattleStarat: result[0],
+            onShot: result[1],
+            onBattleResult: result[2]
+        })
     })
 
 export default router
